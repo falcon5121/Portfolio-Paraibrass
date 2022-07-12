fceed8e4ca38e917c4aaf9b1e52faa3e0ba787a2
falcon5121 <fxemanuel@gmail.com>
2022-07-12 01:06:51 -0300

    Atualização do design da pagina e outras pequenas modificações

---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import "./Menu.css"
+
+
+
+export default function() {
+    return (
+        
+
+        <ul className="caixa">
+                <li className="linhas"></li>
+                <li className="linhas"></li>
+                <li className="linhas"></li>
+            </ul>
+               
+        
+    )
+}
